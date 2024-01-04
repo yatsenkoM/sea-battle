11af5c02fad96d35fe025538d65cb7d517243b75
Maksym Yatsenko <saddas1rambler@gmail.com>
2024-01-04 19:54:20 +0200

    creating ShowingWinnerDialog class

--- app/src/main/java/com/project/seaBattle/BattleActivity.kt
+++ app/src/main/java/com/project/seaBattle/BattleActivity.kt
@@ -1,13 +1,6 @@
 package com.project.seaBattle
 
-import android.app.Dialog
-import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.Window
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.project.seaBattle.databinding.ActivityBattleBinding
@@ -38,7 +31,10 @@ class BattleActivity : AppCompatActivity() {
         binding.battleEditor.setLeftBoard(leftGameBoard)
         binding.battleEditor.setOnGameOver { loser ->
             gameInfoManager.winnerName = if (loser == BoardSide.RIGHT) gameInfoManager.leftPlayerName else gameInfoManager.rightPlayerName
-            gameInfoManager.winnerName?.let { showWinnerDialog(it) }
+            gameInfoManager.winnerName?.let {
+                val showingWinnerDialog = ShowingWinnerDialog(it)
+                showingWinnerDialog.show(supportFragmentManager, "ShowingWinnerDialog")
+            }
             gameInfoManager.resetAllInfo()
         }
     }
@@ -50,21 +46,5 @@ class BattleActivity : AppCompatActivity() {
                 }
             }
         onBackPressedDispatcher.addCallback(this, callback)
-    }
-
-    private fun showWinnerDialog(winnerName: String) {
-        val dialog = Dialog(this)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.dialog_winner_layout)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        val winner: TextView = dialog.findViewById(R.id.winner)
-        winner.text = winnerName
-        val returnStartActivityButton: Button = dialog.findViewById(R.id.returnStartActivity)
-        returnStartActivityButton.setOnClickListener {
-            val intent = Intent(this, StartActivity::class.java)
-            startActivity(intent)
-        }
-        dialog.show()
     }
 }
---
+++ app/src/main/java/com/project/seaBattle/ShowingWinnerDialog.kt
@@ -0,0 +1,46 @@
+package com.project.seaBattle
+
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import com.project.seaBattle.databinding.DialogWinnerLayoutBinding
+
+class ShowingWinnerDialog(private val winnerName: String) : DialogFragment() {
+    @Suppress("ktlint:standard:property-naming")
+    private var _binding: DialogWinnerLayoutBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = DialogWinnerLayoutBinding.inflate(inflater, container, false)
+        val view = binding.root
+        binding.winner.text = winnerName
+        binding.returnStartActivity.setOnClickListener {
+            val intent = Intent(context, StartActivity::class.java)
+            startActivity(intent)
+        }
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return dialog
+    }
+}
